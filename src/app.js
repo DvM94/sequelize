93efd0c667953616c04b65d6e75bf4ac256f8e78
DvM94 <danielvillarmartin@gmail.com>
2021-04-20 08:55:12 +0200

    avances en ejemplo

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
-//servidor
+require('dotenv').config()
 const express = require("express")
 const app = express()
 const rtMain = require('./routers/rtMain')
 const port = process.env.PORT || 3000
+const DB = require('./models/index')
 
 //middlewares
 app.use(express.json())
 
 //enroutador principal
 app.use("/",rtMain)
+
+//base de datos
+DB.authenticate()
+  .then(()=>console.log("BD mySQL arrancado"))
+  .catch(err=>console.log(err))
 
 //arrancar el servidor
 app.listen(port,(err)=>{console.log(`Server run on port ${port}`)})
